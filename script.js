b9f74b086f4a150f1be7d86ecb130a1643532c04
ShanawazGithub <103516489+ShanawazGithub@users.noreply.github.com>
2022-09-19 13:10:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var resume=[{"Name":"Shanawaz.A",
+             "qualification":"B.E",
+             "email":"shanushanawaz50@gmail.com",
+            "Experience":"Fresher",
+        "Address":"ABC",
+    "Domain":"Full stack development",}];
+ //console.log(resume);
+resume.forEach(function() {
+    console.log(resume);
+});
